6f42267e5a19c8da5f4e543240179df885492c6d
angelpray <1136472002@qq.com>
2019-03-12 20:32:19 +0800

    首页header区域开发

--- go-travel/src/pages/home/Home.vue
+++ go-travel/src/pages/home/Home.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>hello world</div>
+  <home-header></home-header>
 </template>
 
 <script>
+import HomeHeader from './components/Header.vue'
 export default {
-  name: 'home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
