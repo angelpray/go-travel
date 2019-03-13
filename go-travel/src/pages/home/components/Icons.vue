945073103d55b66d929857bb52480be8b6844dc0
angelpray <1136472002@qq.com>
2019-03-13 21:26:18 +0800

    推荐组件开发

--- go-travel/src/pages/home/components/Icons.vue
+++ go-travel/src/pages/home/components/Icons.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="icons">
-      <swiper>
-        <swiper-slide v-for="(page, index) of pages" :key="index">
-          <div class="icon" v-for="item of page" :key="item.id">
-            <div class="icon-img">
-              <img class="icon-img-content" :src="item.imgUrl" alt="">
-            </div>
-            <p class="icon-desc">{{item.desc}}</p>
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div
+          class="icon"
+          v-for="item of page"
+          :key="item.id"
+        >
+          <div class='icon-img'>
+            <img class='icon-img-content' :src='item.imgUrl'/>
           </div>
-        </swiper-slide>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
     </swiper>
   </div>
 </template>
@@ -19,55 +23,55 @@ export default {
   data () {
     return {
       iconList: [{
-          id: "001",
-          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
-        }, {
-          id: "002",
-          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
-        }, {
-          id: "003",
-          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
-        }, {
-          id: "004",
-          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
-        }, {
-          id: "005",
-          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
-        }, {
-          id: "006",
-          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
-        }, {
-          id: "007",
-          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
-        }, {
-          id: "008",
-          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
-        }, {
-          id: "009",
-          imgUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
-          desc: "景点门票"
-        }]
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+        desc: '滑雪季'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+        desc: '泡温泉'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+        desc: '动植园'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png',
+        desc: '滑雪季'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png',
+        desc: '泡温泉'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png',
+        desc: '动植园'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png',
+        desc: '一日游'
+      }]
     }
   },
   computed: {
     pages () {
-      const pages = [];
+      const pages = []
       this.iconList.forEach((item, index) => {
-        var page = Math.floor(index/8);
+        const page = Math.floor(index / 8)
         if (!pages[page]) {
           pages[page] = []
         }
         pages[page].push(item)
       })
-      return pages;
+      return pages
     }
   }
 }
@@ -75,36 +79,40 @@ export default {
 
 <style lang="stylus" scoped>
   @import '~styles/variables.styl'
-  @import '~styles/mixin.styl'
+  @import '~styles/mixins.styl'
   .icons >>> .swiper-container
-    width: 100%
     height: 0
     padding-bottom: 50%
-    overflow: hidden
-  .icon
-    height: 0
-    width: 25%
-    padding-bottom: 25%
-    float left
-    overflow hidden
-    position relative
-    .icon-img
-      position absolute
-      top 0
-      right 0
-      left 0
-      bottom .44rem
-      .icon-img-content
-        height 100%
-        display block
-        margin 0 auto
-        padding .1rem
-    .icon-desc
-      position absolute
-      right 0
-      left 0
-      bottom 0
-      text-align center
-      color $darkTextColor
-      ellipsis()
+    touch-action: none;
+  .icons
+    margin-top: .1rem
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
 </style>
---
+++ go-travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class='title'>
+      热销推荐
+    </div>
+    <ul>
+      <li class='item border-bottom' v-for='(item, index) of list' :key='index'>
+        <img class='item-img'
+        :src='item.imgUrl' alt=''>
+        <div class='item-info'>
+          <p class='item-title'>{{item.title}}</p>
+          <p class='item-desc'>{{item.desc}}</p>
+          <button class='item-button'>查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      list: [{
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/34/34170b200db08a5a90.img.jpg_250x250_02eb6e72.jpg',
+        title: '东莞龙凤山庄影视渡假村',
+        desc: '谁说童话里都是骗人的？'
+      }, {
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/34/34170b200db08a5a90.img.jpg_250x250_02eb6e72.jpg',
+        title: '东莞龙凤山庄影视渡假村',
+        desc: '谁说童话里都是骗人的？'
+      }, {
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/34/34170b200db08a5a90.img.jpg_250x250_02eb6e72.jpg',
+        title: '东莞龙凤山庄影视渡假村',
+        desc: '谁说童话里都是骗人的？'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex: 1
+      padding .1rem
+      min-width  0
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color: #ccc
+      ellipsis()
+    .item-button
+      line-height .44rem
+      margin-top .17rem
+      background #ff9300
+      padding 0 0.2rem
+      border-radius .1rem
+      color: #fff
+</style>
