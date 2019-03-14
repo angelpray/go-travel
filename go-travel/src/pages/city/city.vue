686af3441d353ed06061ddf350cc44197ac19586
angelpray <1136472002@qq.com>
2019-03-15 00:17:48 +0800

    城市头部组件

--- go-travel/src/pages/city/city.vue
+++ go-travel/src/pages/city/city.vue
@@ -1,34 +1,17 @@
 <template>
-  <div class="header">
-    城市选择
-    <router-link to="/">
-      <div class="iconfont header-back">&#xe624;</div>
-    </router-link>
-  </div>
+  <city-header></city-header>
 </template>
 
 <script>
+import CityHeader from '@/pages/city/components/Header.vue'
 export default {
-  name: 'City'
+  name: 'City',
+  components: {
+    CityHeader
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import '@~styles/variables.styl'
-  .header
-    line-height $headerHeight
-    height $headerHeight
-    overflow hidden
-    text-align center
-    background $bgcolor
-    color #fff
-    font-size .32rem
-    .header-back
-      position absolute
-      top 0
-      left 0
-      width .64rem
-      text-align center
-      font-size $backIconSize
-      color #fff
+
 </style>
---
+++ go-travel/src/pages/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '@~styles/variables.styl'
+  .header
+    line-height $headerHeight
+    height $headerHeight
+    overflow hidden
+    text-align center
+    background $bgcolor
+    color #fff
+    font-size .32rem
+    .header-back
+      position absolute
+      top 0
+      left 0
+      width .64rem
+      text-align center
+      font-size $backIconSize
+      color #fff
+</style>
